d49f62f949344a023e98171e539423b90b2736fb
闫永俊 <yanyojun@foxmail.com>
2020-06-27 16:11:51 +0800

    Initial commit

---
+++ app/src/main/java/com/plbear/lxc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.plbear.lxc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
